30c1bb7494ca443c5718f297e183b9877c3107c8
Nainish Rai <nainishraisjp@gmail.com>
2024-03-24 14:49:13 +0530

    login btn added on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import "../styles/text-editor.css";
 import "@blocknote/react/style.css";
 import { validateRequest } from "@/lib/lucia/auth";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 export default async function Home() {
   const { user } = await validateRequest();
   if (user) {
@@ -13,6 +15,9 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
       <h1 className="heading1 text-xl font-bold">Welcome back,</h1>
+      <Link href="/login">
+        <Button>Login To Get Started</Button>
+      </Link>
       <Room Editor={<Editor />} roomId="nextjs-yjs-blocknote-advanced" />
     </main>
   );
