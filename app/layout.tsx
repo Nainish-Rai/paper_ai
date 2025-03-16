0b9f3b0c338f730ff6cfc1ef8814b682ed2342ac
Nainish Rai <nainishraisjp@gmail.com>
2025-03-16 21:28:53 +0530

    feat: lucia auth removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "../styles/text-editor.css";
-import { validateRequest } from "@/lib/lucia/auth";
-import { SessionProvider } from "@/lib/providers/SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,17 +10,14 @@ export const metadata: Metadata = {
   description: "A fullstack notion clone with ai",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const sessionData = await validateRequest();
   return (
-    <html lang="en" className="p-0 m-0   ">
-      <SessionProvider value={sessionData}>
-        <body className={inter.className}>{children}</body>
-      </SessionProvider>
+    <html lang="en" className="p-0 m-0">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,10 @@ import Room from "./room/Room";
 import { Editor } from "@/components/Editor";
 import "../styles/text-editor.css";
 import "@blocknote/react/style.css";
-import { validateRequest } from "@/lib/lucia/auth";
-import { redirect } from "next/navigation";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-export default async function Home() {
-  const { user } = await validateRequest();
-  if (user) {
-    redirect("/userdashboard");
-  }
 
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <h1 className="heading1 text-xl font-bold">Welcome back,</h1>
-      <Link href="/login">
-        <Button>Login To Get Started</Button>
-      </Link>
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Room Editor={<Editor />} roomId="nextjs-yjs-blocknote-advanced" />
     </main>
   );
