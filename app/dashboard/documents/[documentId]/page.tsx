1ae998ef124a1b003c85b36333d4ebd4a9f178bd
Nainish Rai <nainishraisjp@gmail.com>
2025-03-23 03:36:28 +0530

    fix: update parameter type in DocumentPage to improve type safety

--- app/dashboard/documents/[documentId]/page.tsx
+++ app/dashboard/documents/[documentId]/page.tsx
@@ -3,7 +3,7 @@ import DocumentPageClient from "./documentPage";
 export default async function DocumentPage({
   params,
 }: {
-  params: Promise<{ documentId: string }>;
+  params: { documentId: string };
 }) {
   const { documentId } = await params;
   return <DocumentPageClient documentId={documentId} />;
