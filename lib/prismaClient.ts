8f3e32cbbb59e914e72e90a5c9cfc202fd4107bf
Nainish Rai <nainishraisjp@gmail.com>
2025-03-23 04:10:21 +0530

    feat: enhance Prisma client initialization for improved logging and environment handling

--- lib/prismaClient.ts
+++ lib/prismaClient.ts
@@ -1,4 +1,24 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+    datasourceUrl:
+      process.env.NODE_ENV === "production"
+        ? process.env.DATABASE_URL
+        : process.env.DIRECT_URL, // Use direct connection in development
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
